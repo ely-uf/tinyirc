1a898fcff4424bfe519dad8868334e81d797ae96
ely-uf <elyahove@student.unit.ua>
2019-08-16 01:20:23 +0300

    Add SIGPIPE SIG_IGN handler.

--- server/main.c
+++ server/main.c
@@ -1,9 +1,11 @@
+#include <signal.h>
 #include "server.h"
 
 int     main(void)
 {
     t_server    server;
 
+    signal(SIGPIPE, SIG_IGN);
     if (server_open(&server) != 0)
         return (1);
     server_serve(&server);
