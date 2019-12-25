935b1dbcbd376461a3ddff59bbed972e0d62159f
ely-uf <elyahove@student.unit.ua>
2019-12-25 14:35:54 +0200

    Add proper response to the QUIT command.

--- server/commands/quit.c
+++ server/commands/quit.c
@@ -4,9 +4,15 @@
 
 int     command_quit(t_conn *user, int argc, char **argv)
 {
+    t_ircmsg    message = {
+        .command = "ERROR",
+        .nparams = 1,
+        .params = { "Bye!" },
+    };
     (void)argc;
     (void)argv;
     LOG(L_INFO, "Command QUIT\n");
+    ircmsg_send(&message, user);
     server_drop(CONN_SERVER(user), user);
     return (0);
 }
