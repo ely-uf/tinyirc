0e8cfe747b7de433606a1a2f856f224b74daf934
ely-uf <elyahove@student.unit.ua>
2020-01-26 14:16:31 +0200

    Add runtime typecheck for vlist operations.

--- include/vlist.h
+++ include/vlist.h
@@ -4,6 +4,8 @@
 # include <stdlib.h>
 # include <pthread.h>
 # include <stdbool.h>
+# include <stdio.h>
+# include <string.h>
 
 typedef struct  s_vlist
 {
@@ -19,14 +21,26 @@ typedef struct  s_vlist
 # define VLIST_OF(type) t_vlist
 # define VLIST_ADD(type, v, value) ({   \
         type    __tmp_val = value;      \
+        VLIST_TYPECHECK(v, type);       \
         vlist_add_gen(v, &__tmp_val);   \
     })
 # define VLIST_DELETE(type, v, value) ({    \
         type    __tmp_val = value;          \
+        VLIST_TYPECHECK(v, type);           \
         vlist_delete_gen(v, &__tmp_val);    \
     })
 # define VLIST_INIT(type, v) vlist_init(v, sizeof(type), #type)
 
+# define VLIST_TYPECHECK(v, el_type) ({                         \
+        const char *__v_type = (v)->type;                       \
+        if (strcmp(__v_type, #el_type)) {                       \
+            printf("%s: Type mismatch for vlist operation!\n"   \
+                   "\tExpected: %s\n"                           \
+                   "\tGot: %s\n",                               \
+                   __func__, #el_type, __v_type);               \
+            abort();                                            \
+        }                                                       \
+    })
 int     vlist_init(t_vlist *vlist, size_t elem_size, char *type);
 int     vlist_add_gen(t_vlist *vlist, void *value_address);
 int     vlist_delete_gen(t_vlist *vlist, void *value_address);
